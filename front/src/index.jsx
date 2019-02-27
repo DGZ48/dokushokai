12c0e1f332d813912781e6abd7e8a48d5bb5c094
inabajunmr <inabajun.for.regi@gmail.com>
2019-02-28 01:10:36 +0900

    init

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import firebase from './firebase'
 
 class App extends React.Component {
+
+	state = {
+		user: null
+	}
+
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(user => {
+			this.setState({ user })
+		})
+	}
+
+	login() {
+		const provider = new firebase.auth.GoogleAuthProvider()
+		firebase.auth().signInWithRedirect(provider)
+	}
+
+	logout() {
+		firebase.auth().signOut()
+	}
+
 	constructor(props) {
 		super(props)
 		this.state = { message: 'something' }
 	}
 
-	onChange(e) {
-		this.setState({message: e.target.value})
-	}
 	render() {
 		return (
-			<div>
-				<input type="text" onChange = { this.onChange.bind(this)} />
-				<p>{ this.state.message }</p>
+			<div className="App">
+				<p className="App-intro">
+					UID: {this.state.user && this.state.user.uid}
+				</p>
+
+				{this.state.user ? (
+					<button onClick={this.logout}>Google Logout</button>
+				) : (
+						<button onClick={this.login}>Google Login</button>
+					)}
 			</div>
 		)
 	}
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
 
